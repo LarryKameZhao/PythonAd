17b219831293d9edc1faad9d31129dd2c4bc482d
LarryKameZhao <larryzhao1994@outlook.com>
2018-07-19 08:13:24 +0800

    thread ssemaphor

---
+++ ch11/thread_semaphore.py
@@ -0,0 +1,36 @@
+#Semaphore用于控制进入数量的锁
+#文件  读、写，写一般是用于一个线程写，读允许多个
+
+import threading
+import time
+
+
+class HtmlSpider(threading.Thread):
+    def __init__(self,url,sem):
+        super().__init__()
+        self.url = url
+        self.sem = sem
+
+    def run(self):
+        time.sleep(2)
+        print('GOT HTML TEXT SUCCESS')
+        self.sem.release()
+
+
+class UrlProducer(threading.Thread):
+
+    def __init__(self,sem):
+        super().__init__()
+        self.sem = sem
+
+    def run(self):
+        for i in range(20):
+            self.sem.acquire()
+            html_thread = HtmlSpider('htts://baidu.com/{}'.format(i),self.sem)
+            html_thread.start()
+
+if __name__ == "__main__":
+    sem = threading.Semaphore(3)
+    #并发数量3
+    url_producer = UrlProducer(sem)
+    url_producer.start()
